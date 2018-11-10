0e431dd5fcbc92c4199768644f05169f5118b12e
Jack <731547576@qq.com>
2018-11-10 15:14:31 +0000

    List topics created by user

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,4 +26,20 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function topics()
+    {
+        return $this->hasMany(Topic::class);
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
