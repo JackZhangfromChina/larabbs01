bb02c2474576f9e26661c2f71c018ffb9e4a645c
Jack <731547576@qq.com>
2018-11-10 14:49:00 +0000

    Category topic list

--- public/js/app.js
+++ public/js/app.js
@@ -43301,7 +43301,7 @@ if (false) {
 /* 42 */
 /***/ (function(module, exports) {
 
-// removed by extract-text-webpack-plugin
+throw new Error("Module build failed: ModuleBuildError: Module build failed: \n.topics-index-page, .categories-show-page {\n                                         ^\n      Invalid CSS after \"...ies-show-page {\": expected \"}\", was \"\"\n      in /home/vagrant/Code/larabbs/resources/assets/sass/app.scss (line 105, column 43)\n    at runLoaders (/home/vagrant/Code/larabbs/node_modules/webpack/lib/NormalModule.js:195:19)\n    at /home/vagrant/Code/larabbs/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /home/vagrant/Code/larabbs/node_modules/loader-runner/lib/LoaderRunner.js:230:18\n    at context.callback (/home/vagrant/Code/larabbs/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Object.asyncSassJobQueue.push [as callback] (/home/vagrant/Code/larabbs/node_modules/sass-loader/lib/loader.js:55:13)\n    at Object.done [as callback] (/home/vagrant/Code/larabbs/node_modules/neo-async/async.js:8077:18)\n    at options.error (/home/vagrant/Code/larabbs/node_modules/node-sass/lib/index.js:294:32)");
 
 /***/ })
 /******/ ]);
