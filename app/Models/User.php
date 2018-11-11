28290e6229826e424abb137c75a0c0425dfc2d1d
mmws <731547576@qq.com..Mac>
2018-11-11 20:49:28 +0800

    Author can delete topic

--- app/Models/User.php
+++ app/Models/User.php
@@ -32,6 +32,11 @@ class User extends Authenticatable
         return $this->hasMany(Topic::class);
     }
 
+    public function isAuthorOf($model)
+    {
+        return $this->id == $model->user_id;
+    }
+
 
 
 
