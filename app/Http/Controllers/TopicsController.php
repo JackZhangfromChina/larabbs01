592c269772657fffa4eeabf070608d0da56495c2
Jack <731547576@qq.com>
2018-11-13 05:49:02 +0000

    User can delete reply

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Topic;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -10,6 +11,7 @@ use Auth;
 use App\Handlers\ImageUploadHandler;
 use App\Models\User;
 use App\Models\Link;
+
 class TopicsController extends Controller
 {
     public function __construct()
@@ -17,91 +19,83 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index(Request $request, Topic $topic)
-	{
-        $topics = $topic->withOrder($request->order)->paginate(20);
-		return view('topics.index', compact('topics'));
-	}
-
-    public function show(Request $request,Topic $topic)
+    public function index(Request $request, Topic $topic)
     {
-//        / URL 矫正
-        if ( ! empty($topic->slug) && $topic->slug != $request->slug) {
+        $topics = $topic->withOrder($request->order)->paginate(20);
+        return view('topics.index', compact('topics'));
+    }
+
+    public function show(Request $request, Topic $topic)
+    {
+//        URL 矫正
+        if (!empty($topic->slug) && $topic->slug != $request->slug) {
             return redirect($topic->link(), 301);
         }
         return view('topics.show', compact('topic'));
     }
 
-	public function create(Topic $topic)
-	{
+    public function create(Topic $topic)
+    {
         $categories = Category::all();
-		return view('topics.create_and_edit', compact('topic','categories'));
-	}
+        return view('topics.create_and_edit', compact('topic', 'categories'));
+    }
 
-	public function store(TopicRequest $request,Topic $topic)
-	{
+    public function store(TopicRequest $request, Topic $topic)
+    {
 //		$topic = Topic::create($request->all());
         $topic->fill($request->all());
         $topic->user_id = Auth::id();
         $topic->save();
-		return redirect()->to($topic->link())->with('message', '成功创建话题');
-	}
+        return redirect()->to($topic->link())->with('message', '成功创建话题');
+    }
 
-	public function edit(Topic $topic)
-	{
+    public function edit(Topic $topic)
+    {
         $this->authorize('update', $topic);
         $categories = Category::all();
         return view('topics.create_and_edit', compact('topic', 'categories'));
-	}
-
-	public function update(TopicRequest $request, Topic $topic)
-	{
-		$this->authorize('update', $topic);
-		$topic->update($request->all());
-
-		return redirect()->to($topic->link())->with('message', '更新成功');
-	}
-
-	public function destroy(Topic $topic)
-	{
-		$this->authorize('destroy', $topic);
-		$topic->delete();
-
-		return redirect()->route('topics.index')->with('message', '删除成功');
-	}
-
-    /**
-     * @param Request $request
-     * @param ImageUploadHandler $uploader
-     * @return array
-     */
-    public function uploadImage(Request $request, ImageUploadHandler $uploader)
-    {
-        // 初始化返回数据，默认是失败的
-        $data = [
-            'success'   => false,
-            'msg'       => '上传失败!',
-            'file_path' => ''
-        ];
-        // 判断是否有上传文件，并赋值给 $file
-        if ($file = $request->upload_file) {
-            // 保存图片到本地
-            $result = $uploader->save($request->upload_file, 'topics', \Auth::id(), 1024);
-            // 图片保存成功的话
-            if ($result) {
-                $data['file_path'] = $result['path'];
-                $data['msg']       = "上传成功!";
-                $data['success']   = true;
-            }
-        }
-        return $data;
     }
 
+    public function update(TopicRequest $request, Topic $topic)
+    {
+        $this->authorize('update', $topic);
+        $topic->update($request->all());
 
+        return redirect()->to($topic->link())->with('message', '更新成功');
+    }
 
-
-
-
+    public function destroy(Topic $topic, Reply $reply)
+    {
+        $this->authorize('destroy', $topic);
+        $topic->delete();
+        return redirect()->to($reply->topic->link())->with('success', '成功删除回复！');
+    }
+        /**
+         * @param Request $request
+         * @param ImageUploadHandler $uploader
+         * @return array
+         */
+        function uploadImage(Request $request, ImageUploadHandler $uploader)
+        {
+            // 初始化返回数据，默认是失败的
+            $data = [
+                'success' => false,
+                'msg' => '上传失败!',
+                'file_path' => ''
+            ];
+            // 判断是否有上传文件，并赋值给 $file
+            if ($file = $request->upload_file) {
+                // 保存图片到本地
+                $result = $uploader->save($request->upload_file, 'topics', \Auth::id(), 1024);
+                // 图片保存成功的话
+                if ($result) {
+                    $data['file_path'] = $result['path'];
+                    $data['msg'] = "上传成功!";
+                    $data['success'] = true;
+                }
+            }
+            return $data;
+        }
 
 
 
