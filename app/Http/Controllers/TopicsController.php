21be7a679f3f64be0c467dd3042f490ebfd32e3f
Jack <731547576@qq.com>
2018-11-14 03:51:44 +0000

    Sidebar links

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -19,12 +19,13 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-    public function index(Request $request, Topic $topic,User $user)
+    public function index(Request $request, Topic $topic,User $user, Link $link)
     {
         $topics = $topic->withOrder($request->order)->paginate(20);
         $active_users = $user->getActiveUsers();
+        $links = $link->getAllCached();
 //        dd($active_users);
-        return view('topics.index', compact('topics', 'active_users'));
+        return view('topics.index', compact('topics', 'active_users','links'));
     }
 
     public function show(Request $request, Topic $topic)
