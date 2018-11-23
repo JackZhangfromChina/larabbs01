5ac8d757601fdda8b4a615460bae7ace50229630
Jack <731547576@qq.com>
2018-11-23 07:24:24 +0000

    user update

---
+++ app/Http/Controllers/Api/ImagesController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Image;
+use Illuminate\Http\Request;
+use App\Handlers\ImageUploadHandler;
+use App\Transformers\ImageTransformer;
+use App\Http\Requests\Api\ImageRequest;
+class ImagesController extends Controller
+{
+    public function store(ImageRequest $request, ImageUploadHandler $uploader, Image $image)
+    {
+        $user = $this->user();
+
+        $size = $request->type == 'avatar' ? 362 : 1024;
+        $result = $uploader->save($request->image, str_plural($request->type), $user->id, $size);
+
+        $image->path = $result['path'];
+        $image->type = $request->type;
+        $image->user_id = $user->id;
+        $image->save();
+
+        return $this->response->item($image, new ImageTransformer())->setStatusCode(201);
+    }
+
+    /**
+     * @param UserRequest $request
+     * @return \Dingo\Api\Http\Response
+     */
+    public function update(UserRequest $request)
+    {
+        $user = $this->user();
+
+        $attributes = $request->only(['name', 'email', 'introduction']);
+
+        if ($request->avatar_image_id) {
+            $image = Image::find($request->avatar_image_id);
+
+            $attributes['avatar'] = $image->path;
+        }
+        $user->update($attributes);
+
+        return $this->response->item($user, new UserTransformer());
+    }
+}
